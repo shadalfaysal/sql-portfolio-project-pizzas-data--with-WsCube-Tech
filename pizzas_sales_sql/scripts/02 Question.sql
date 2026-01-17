2ace9b1296fec4dca1c74c12de3a55093e01b85c
Shad Al Faysal <mdshadalfaysal@gmail.com>
2026-01-17 16:11:06 +0600

    Update 02 Question.sql

--- pizzas_sales_sql/scripts/02 Question.sql
+++ pizzas_sales_sql/scripts/02 Question.sql
@@ -1,4 +1,6 @@
+==============================================
 --Retrieve the total number of orders placed.
+==============================================
  
 select 
 count (order_id) as total_orders
